827e54abbc135cbf5d03ad7b800a5a2a6dd5da77
Sahin Zaybak <sahin.zaybak@cimri.com>
2021-09-19 16:46:19 +0300

    more product demo

--- pages/index.jsx
+++ pages/index.jsx
@@ -53,6 +53,62 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div className="product-detail__more pb-5">
+        <div className="row">
+          <div className="col-md-3">
+            <div className="product-detail__more-item">
+              <img src="https://footwearnews.com/wp-content/uploads/2019/05/met-gala-2019-converse.jpg" alt="" />
+              <h2 className="product-detail__more-title">En renkli AYAKAKABI çeşitleri</h2>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="product-detail__more-item">
+              <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/white-tee-1623337322.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*" alt="" />
+              <h2 className="product-detail__more-title">Yaza özel T-SHIRT çeşitleri</h2>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="product-detail__more-item">
+              <img src="https://glorytrends.com/wp-content/uploads/2020/04/womens-handbags-2021.jpg" alt="" />
+              <h2 className="product-detail__more-title">Deri ÇANTA çeşitleri</h2>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="product-detail__more-item">
+              <img src="https://ktnimg.mncdn.com/mnresize/406/534/product-images/1KAK68167PW001/1500Wx1969H/1KAK68167PW001_G01_zoom1_V02.jpg" alt="" />
+              <h2 className="product-detail__more-title">En güzel BLUZ çeşitleri</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="product-detail__more pb-5">
+        <div className="row">
+          <div className="col-md-3">
+            <div className="product-detail__more-item">
+              <img src="https://footwearnews.com/wp-content/uploads/2019/05/met-gala-2019-converse.jpg" alt="" />
+              <h2 className="product-detail__more-title">En renkli AYAKAKABI çeşitleri</h2>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="product-detail__more-item">
+              <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/white-tee-1623337322.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*" alt="" />
+              <h2 className="product-detail__more-title">Yaza özel T-SHIRT çeşitleri</h2>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="product-detail__more-item">
+              <img src="https://glorytrends.com/wp-content/uploads/2020/04/womens-handbags-2021.jpg" alt="" />
+              <h2 className="product-detail__more-title">Deri ÇANTA çeşitleri</h2>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="product-detail__more-item">
+              <img src="https://ktnimg.mncdn.com/mnresize/406/534/product-images/1KAK68167PW001/1500Wx1969H/1KAK68167PW001_G01_zoom1_V02.jpg" alt="" />
+              <h2 className="product-detail__more-title">En güzel BLUZ çeşitleri</h2>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="product-list">
         {productList.map((product, index) => (
           <HomeProduct productList={product} key={index} />
